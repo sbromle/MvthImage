0ff2ca2ff59b13f7305ff0e7b3c46290d91e1ae5
Sam Bromley <sbromley@pangeosubsea.com>
2007-06-12 21:23:38 +0000

    Fix diffimages.
    git-archimport-id: sbromley@pangeosubsea.com--mvth-2007a/mvthimage--sam-dev--2.0--patch-23

--- src/diff/diff_fltr.c
+++ src/diff/diff_fltr.c
@@ -22,10 +22,14 @@
 int diff_fltr(image_t *img0, image_t *img1, image_t *img2)
 {
 	float *data[3]={NULL,NULL,NULL};
-	image_t *img[3]={img0,img1,img2};
+	image_t *img[3]={NULL,NULL,NULL};
 	int h[3],w[3],bands[3];
 	int i,j,k;
-	int offset[3];
+	unsigned int offset[3];
+
+	img[0]=img0;
+	img[1]=img1;
+	img[2]=img2;
 
 	if (img[0]==NULL || img[1]==NULL || img[2]==NULL)
 	{
@@ -33,7 +37,7 @@ int diff_fltr(image_t *img0, image_t *img1, image_t *img2)
 		return -1;
 	}
 
-	if (img[2]->bands<img[0]->bands || img[2]->bands<img[1]->bands)
+	if (img[2]->bands < img[0]->bands || img[2]->bands < img[1]->bands)
 	{
 		fprintf(stderr,"diff_fltr(): Result image needs at least as many colors as src images!\n");
 		return -1;
@@ -41,9 +45,12 @@ int diff_fltr(image_t *img0, image_t *img1, image_t *img2)
 
 	for (i=0;i<3;i++)
 	{
-		h[i]=img[i]->h; w[i]=img[i]->w; bands[i]=img[i]->bands;
+		h[i]=img[i]->h;
+		w[i]=img[i]->w;
+		bands[i]=img[i]->bands;
 		data[i]=img[i]->data;
 	}
+	memset(data[2],0,h[2]*w[2]*bands[2]*sizeof(float));
 
 	for (j=0;j<h[2];j++)
 	{
@@ -51,9 +58,9 @@ int diff_fltr(image_t *img0, image_t *img1, image_t *img2)
 		for (i=0;i<w[2];i++)
 		{
 			if (i>w[0]-1 && i>w[1]-1) break;
-			offset[0]=bands[0]*(j*w[2]+i);
-			offset[1]=bands[1]*(j*w[0]+i);
-			offset[2]=bands[2]*(j*w[1]+i);
+			offset[0]=(unsigned int)(bands[0]*(j*w[0]+i));
+			offset[1]=(unsigned int)(bands[1]*(j*w[1]+i));
+			offset[2]=(unsigned int)(bands[2]*(j*w[2]+i));
 			float val[2][4]={{0,0,0,0},{0,0,0,0}}; /* lookup table for image values */
 
 			if (j<h[0] && j<h[1] && i<w[0] && i<w[1])
@@ -61,22 +68,26 @@ int diff_fltr(image_t *img0, image_t *img1, image_t *img2)
 				/* in both images */
 				for (k=0;k<bands[0];k++) val[0][k]=*(data[0]+offset[0]+k);
 				for (k=0;k<bands[1];k++) val[1][k]=*(data[1]+offset[1]+k);
-				for (k=0;k<bands[2];k++)
-					*(data[2]+offset[2]+k)=(float)fabs(val[0]-val[1]);
+				for (k=0;k<bands[2];k++) {
+					*(data[2]+offset[2]+k)=(float)fabs(val[0][k]-val[1][k]);
+				}
 			}
 			else if ((j>h[0]-1 && i<w[1]) || (i>w[0]-1 && j<h[1]) )
 			{
-				/* outside first image */
+				/* in second image */
 				for (k=0;k<bands[1];k++) val[1][k]=*(data[1]+offset[1]+k);
 				for (k=0;k<bands[2];k++) *(data[2]+offset[2]+k)=val[1][k];
 			}
 			else if ( (j>h[1]-1 && i<w[0]) || (i>w[1]-1 && j<h[0]) )
 			{
-				/* outside second image */
+				/* in first image */
 				for (k=0;k<bands[0];k++) val[0][k]=*(data[0]+offset[0]+k);
 				for (k=0;k<bands[2];k++) *(data[2]+offset[2]+k)=val[0][k];
 			}
 			/* else were are outside both images */
+			else {
+				for (k=0;k<bands[2];k++) *(data[2]+offset[2]+k)=0.0;
+			}
 		}
 	}
 	return 0;
