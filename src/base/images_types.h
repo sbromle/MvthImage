ce92e9da9d75415e9ecd11c5b0cc9c6b18bbc605
Sam Bromley <sbromley@pangeosubsea.com>
2010-03-29 12:16:27 -0230

    Modify IMG_T_GET_X macros.

--- src/base/images_types.h
+++ src/base/images_types.h
@@ -24,7 +24,8 @@ typedef struct image_s
 	char name[128]; /**< name of image in image context array */
 } image_t;
 
-#define IMG_T_GET_PTR(IMG,W,H,D) ((IMG)->data+(IMG)->bands*((W)+(IMG)->w*((H)+(IMG)->h*(D))))
+#define IMG_T_GET_PTR(IMG,W,H,D,T) (T+(IMG)->data+(IMG)->bands*((W)+(IMG)->w*((H)+(IMG)->h*(D))))
+#define IMG_T_GET_VALUE(IMG,W,H,D,T) (((IMG)->data+(IMG)->bands*((W)+(IMG)->w*((H)+(IMG)->h*(D))))[(T)])
 
 /* We should really write the filters to use image blocks,
  * that way we can easily draw to subregions, without needing
