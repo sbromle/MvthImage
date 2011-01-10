f7c66b68341a71e8455492e2801260e106f6a31c
Sam Bromley <sbromley@pangeosubsea.com>
2011-01-10 11:32:06 -0330

    viewimage: Become stand-alone again. Moved other stuff to btguitools.

--- scripts/viewimage.tcl
+++ scripts/viewimage.tcl
@@ -21,105 +21,13 @@ proc yscrollNameFromImg {img} {
 	return [toplevelNameFromImg $img].y
 }
 
-proc panedwindowNameFromImg {img} {
-	return [toplevelNameFromImg $img].pw
-}
-
 proc imageNameFromImg {img} {
 	return my#image_$img
 }
 
 
-proc __build_world_view_frame {w img} {
-	if {![info exists ::viewimage::vparams($img,Eaxis,min)]} {
-		set ::viewimage::vparams($img,Eaxis,min) 0;
-		set ::viewimage::vparams($img,Eaxis,width) 1;
-		set ::viewimage::vparams($img,Naxis,min) 0;
-		set ::viewimage::vparams($img,Naxis,width) 1;
-		set ::viewimage::vparams($img,Aaxis,min) 0;
-		set ::viewimage::vparams($img,Aaxis,width) 1;
-	}
-	set wvlf [labelframe $w -text "World View"];
-	set col 0;
-	set row 1;
-	foreach n {{Top SW corner} width} {
-		set l [label ${wvlf}.l$n -text $n -justify right];
-		grid $l -column $col -row $row -sticky news;
-		incr row;
-	}
-	set col 1;
-	set row 0;
-	foreach dir {E N A} {
-		set l [label ${wvlf}.l$dir -text $dir];
-		grid $l -row 0 -column $col -sticky n;
-		set row 1;
-		# FIXME: viewimage spinbox should not call out to vprofile command;
-		# FIXME: I'll have to fix this using traces on the vprofile side;
-		# FIXME: Leaving it here for now during development;
-		set s [spinbox ${wvlf}.s${dir}min \
-			-from -99999999 -to 9999999 -increment 0.05 \
-			-format %.2f -width 12 \
-			-command ::vprofile::update_view_if_live -justify right];
-		$s configure -textvariable ::viewimage::vparams($img,${dir}axis,min);
-		grid $s -row $row -column $col -sticky news;
-		incr row;
-		set s [spinbox ${wvlf}.s${dir}width \
-			-from 0.05 -to 1000000 -increment 0.05 \
-			-format %.2f -width 7 \
-			-command ::vprofile::update_view_if_live -justify right];
-		$s configure -textvariable ::viewimage::vparams($img,${dir}axis,width);
-		grid $s -row $row -column $col -sticky news;
-		incr col;
-	}
-	return $wvlf;
-}
-
-proc __build_image_region_frame {w img} {
-	if {![info exists ::viewimage::vparams($img,xaxis,min)]} {
-		set ::viewimage::vparams($img,xaxis,min) 0;
-		set ::viewimage::vparams($img,xaxis,width) 1;
-		set ::viewimage::vparams($img,yaxis,min) 0;
-		set ::viewimage::vparams($img,yaxis,width) 1;
-	}
-	set wvlf [labelframe $w -text "Image Region"];
-	set col 0;
-	set row 1;
-	foreach n {x y} {
-		set l [label ${wvlf}.l$n -text $n -justify right];
-		grid $l -column $col -row $row -sticky news;
-		incr row;
-	}
-	set col 0;
-	set row 1;
-	foreach n {min width} {
-		set l [label ${wvlf}.l$n -text $n];
-		grid $l -row 0 -column $col -sticky n;
-		set row 1;
-		# FIXME: viewimage spinbox should not call out to vprofile command;
-		# FIXME: I'll have to fix this using traces on the vprofile side;
-		# FIXME: Leaving it here for now during development;
-		set s [spinbox ${wvlf}.sx${n} \
-			-from 0 -to 1 -increment 0.02 \
-			-format %.2f -width 4 \
-			-command ::vprofile::update_view_if_live -justify right];
-		$s configure -textvariable ::viewimage::vparams($img,xaxis,$n);
-		grid $s -row $row -column $col -sticky news;
-		incr row;
-		set s [spinbox ${wvlf}.sy${n} \
-			-from 0 -to 1 -increment 0.02 \
-			-format %.2f -width 4 \
-			-command ::vprofile::update_view_if_live -justify right];
-		$s configure -textvariable ::viewimage::vparams($img,yaxis,$n);
-		grid $s -row $row -column $col -sticky news;
-		incr col;
-	}
-	return $wvlf;
-}
-
 proc viewimage {img} {
 	set t [toplevel [toplevelNameFromImg $img]]
-	set pw [panedwindow [panedwindowNameFromImg $img] -orient vertical -showhandle 1];
-	set tf [frame $t.tf];
 	set c [canvas [canvasNameFromImg $img]]
 	set x [scrollbar [xscrollNameFromImg $img] -ori hori -command [list $c xview]]
 	set y [scrollbar [yscrollNameFromImg $img] -ori vert -command [list $c yview]]
@@ -127,20 +35,12 @@ proc viewimage {img} {
 	set i [image create photo [imageNameFromImg $img]]
 	$c create image 0 0 -image $i -anchor nw -tag img
 	blitimage $img $i
-	grid $c $y -in $tf -sticky news
-	grid $x -in $tf -sticky ew
-	grid rowconfig $tf 0 -weight 1
-	grid columnconfig $tf 0 -weight 1
+	grid $c $y -in $t -sticky news
+	grid $x -in $t -sticky ew
+	grid rowconfig $t 0 -weight 1
+	grid columnconfig $t 0 -weight 1
 	$c config -scrollregion [$c bbox all]
 	$c config -cursor trek
-	label $t.testlabel -text "HI"
-	set ff [frame $t.ff];
-	set wvf [__build_world_view_frame $t.wvlf $img];
-	set ir [__build_image_region_frame $t.irf $img];
-	pack $wvf $ir -in $ff -anchor nw -side left;
-	$pw add $ff -sticky nw;
-	$pw add $tf -minsize 1i -padx 0 -pady 6 -sticky news;
-	pack $pw -expand yes -fill both;
 	return $t;
 }
 
